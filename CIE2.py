e9e91709c123a99de5ab2abfc35f42cdb791d3fd
Adityaptil51 <adityaapatil51@gmail.com>
2025-11-29 11:44:41 +0530

    save

--- CIE2.py
+++ CIE2.py
@@ -1,14 +1,10 @@
 import sys
 
 if len(sys.argv) == 3:
-    try:
         print("Usage: python CIE2.py <distance> <time>")
         distance = float(sys.argv[1])
         time = float(sys.argv[2])
-    except ValueError:
-        print("Invalid parameters! Using default values.")
-        distance = 1000
-        time = 260
+        sys.exit(2)
 else:
     distance = 1000
     time = 260
