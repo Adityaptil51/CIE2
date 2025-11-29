713314f2324b4978fead25e58633115215ef1b48
Adityaptil51 <adityaapatil51@gmail.com>
2025-11-29 11:39:58 +0530

    save

--- CIE2.py
+++ CIE2.py
@@ -1,15 +1,17 @@
 import sys
 
 if len(sys.argv) == 3:
-    print("Usage: python CIE2.py <distance> <time>")
-    script_name = sys.argv[0]
-    distance = float(sys.argv[1])
-    time = float(sys.argv[2])
-    sys.exit(1)
+    try:
+        print("Usage: python CIE2.py <distance> <time>")
+        distance = float(sys.argv[1])
+        time = float(sys.argv[2])
+    except ValueError:
+        print("Invalid parameters! Using default values.")
+        distance = 1000
+        time = 260
 else:
     distance = 1000
     time = 260
 
 speed = distance / time
-
 print(f"The calculated speed = {speed:.2f}")
