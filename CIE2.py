c26b131823d7b044378fec8a95bf83b364606edc
Adityaptil51 <adityaapatil51@gmail.com>
2025-11-29 11:07:45 +0530

    save

--- CIE2.py
+++ CIE2.py
@@ -3,6 +3,7 @@ import sys
 if len(sys.argv) == 3:
     distance = float(sys.argv[1])
     time = float(sys.argv[2])
+    sys.exit(1)
 else:
     print("Usage: python CIE2.py <distance> <time>")
     distance = 1000
