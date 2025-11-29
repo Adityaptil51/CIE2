2b2cf51f260f11cb46f587d5201e323ba7470344
Adityaptil51 <adityaapatil51@gmail.com>
2025-11-29 10:12:56 +0530

    save

---
+++ CIE2.py
@@ -0,0 +1,16 @@
+import sys
+
+if (sys.argv == 4):
+    script_name = (sys.argv[0])
+    distance = float(sys.argv[1])
+    time = float(sys.argv[2])
+    speed = float(sys.argv[3])
+
+else: 
+    script_name = (sys.argv[0])
+    distance = 1000
+    time = 260
+
+    speed = distance / time
+
+    print("The calculated speed is: ",{speed})
