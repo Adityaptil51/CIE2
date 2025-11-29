7d72ebe551d680edc2d4fcf4c7856b0a31eef71d
Adityaptil51 <adityaapatil51@gmail.com>
2025-11-29 11:52:15 +0530

    save

--- CIE2.py
+++ CIE2.py
@@ -1,15 +1,15 @@
 import sys
 
 if len(sys.argv) == 3:
-        print("Usage: python CIE2.py <distance> <time>")
-        distance = float(sys.argv[1])
-        time = float(sys.argv[2])
-
-        speed = list(map(float, sys.argv[1:3]))
-        sys.exit(1)
+    print("Usage: python CIE2.py <distance> <time>")
+    script_name = sys.argv[0]
+    distance = float(sys.argv[1])
+    time = float(sys.argv[2])
+    sys.exit(1)
 else:
     distance = 1000
     time = 260
 
 speed = distance / time
+
 print(f"The calculated speed = {speed:.2f}")
